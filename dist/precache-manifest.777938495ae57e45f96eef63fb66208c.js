1caa305e4b9f6c37919343ea53788a659e1b1eae
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-07-26 19:49:46 -0300

    build gerada para a pasta /dist e express configurado para servir o sistema da pasta /dist

---
+++ dist/precache-manifest.777938495ae57e45f96eef63fb66208c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b361b0653d3506462ab3",
+    "url": "/js/chunk-vendors.e5f25446.js"
+  },
+  {
+    "revision": "696bb51144b9a732f090",
+    "url": "/js/app.b98714d3.js"
+  },
+  {
+    "revision": "2a750715c5bf4053d8fa47e76d4621f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "b361b0653d3506462ab3",
+    "url": "/css/chunk-vendors.62af2b2c.css"
+  },
+  {
+    "revision": "696bb51144b9a732f090",
+    "url": "/css/app.da0de442.css"
+  }
+];
