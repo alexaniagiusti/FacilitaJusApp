9a7a9adb1fdaccd7515954d1f898495ee3c80e04
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-08-10 00:27:32 -0300

    validação do formulário de cadastro

--- src/pages/Cadastro.vue
+++ src/pages/Cadastro.vue
@@ -15,6 +15,7 @@
               <v-btn
               flat
               color="#43A047"
+              to="/"
               >
               <h4 class="font-weight-light pr-2 white--text">Login</h4>
                 <v-icon
@@ -34,14 +35,15 @@
 
           <v-flex pa-2 xs12>
             <v-text-field
+              :rules="nameRules"
               filled
               v-model="name"
               label="Nome"
               required
-              rules=""
             >
             </v-text-field>
             <v-text-field
+              :rules="emailRules"
               filled
               v-model="email"
               label="E-mail"
@@ -49,30 +51,41 @@
             >
             </v-text-field>
             <v-text-field
+              :rules="cpfRules"
               filled
               v-model="cpf"
               label="CPF"
             >
             </v-text-field>
             <v-text-field
+              :append-icon="showPass ? 'visibility' : 'visibility_off'"
+              :type="showPass ? 'text' : 'password'"
+              :rules="senhaRules"
               filled
               v-model="password"
               label="Senha"
-              type="password"
+              @click:append="showPass = !showPass"
             >
             </v-text-field>
             <v-text-field
+              :append-icon="showPass ? 'visibility' : 'visibility_off'"
+              :type="showPass ? 'text' : 'password'"
+              :rules="confirmaSenhaRules"
+              :hint="passwordHint"
+              @click:append="showPass = !showPass"
               filled
               v-model="confirmPassword"
               label="Confirme a senha"
-              type="password"
             >
             </v-text-field>
           <v-btn
             block
+            :disabled="!passwordOK"
             color="#43A047"
+            @click="cadastraUsuario"
           >
             <span class="white--text font-weight-light">Cadastrar</span>
+            <v-progress-circular v-if="carregandoCadastrar" size="18" indeterminate color="white" class="ml-2"></v-progress-circular>
           </v-btn>
           </v-flex>
         </v-form>
@@ -90,12 +103,38 @@ import 'moment/locale/pt-br';
 export default {
   data() {
     return {
+      cadastroForm: false,
       name: '',
       cpf: '',
       email: '',
       password: '',
+      passwordHint: '',
+      confirmPassword: '',
+      passwordOK: false,
       carregandoCadastrar: false,
+      showPass: false,
+      nameRules: [
+        v => !!v || 'Informe o seu nome por favor' ],
+      emailRules: [
+        v => !!v || 'Informe o seu e-mail' ],
+      cpfRules: [
+        v => !!v || 'CPF é obrigatório' ],
+      senhaRules: [
+        v => !!v || 'Senha é obrigatório' ],
+      confirmaSenhaRules: [
+        v => !!v || 'Corfirmar a senha é obrigatório' ]
     };
+  },
+  watch: {
+    confirmPassword(val) {
+      if(val === this.password) {
+        this.passwordOK = true
+        this.passwordHint = 'As senhas correspondem'
+      } else {
+        this.passwordOK = false
+        this.passwordHint = 'As senhas não correspondem'
+      }
+    }
   },
   methods: {
     cadastraUsuario() {
@@ -104,30 +143,35 @@ export default {
 
         const { name } = this;
         const { email } = this;
-        const { password } = this;
         const { cpf } = this;
+        const { password } = this;
 
         const data = {
           name,
           email,
-          password,
           cpf,
+          password,
         };
 
-        const headers = {
-          'Content-Type': 'application/json',
-        };
+         const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      };
 
-        axios.post('https://facilita-jus-api.herokuapp.com/api/v1/users', data, headers)
+        axios.post(this.$store.getters.api + '/api/v1/users', data, headers)
           .then((res) => {
             this.$store.dispatch('snackbar_success', 'Cadastrado com sucesso!');
             this.$router.push('/');
+            this.carregandoCadastrar = true;
+
           })
           .catch((erro) => {
             this.$store.dispatch('snackbar_error', `Erro ao cadastrar: ${erro}`);
+            this.carregandoCadastrar = true;
           });
       } else {
-        this.$store.dispatch('SNACKBAR_ERROR', 'Preencha corretamente os campos');
+        this.$store.dispatch('snackbar_error', 'Preencha corretamente os campos');
+        this.carregandoCadastrar = true;
       }
     },
   },
