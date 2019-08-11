cdbf9f428f66276e8e93cb664e81c6e64905f385
snplustecnologia <snplustecnologia@gmail.com>
2019-08-11 16:32:08 -0300

    correção na verificação de usuário logado

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,49 @@
 <template>
   <v-app app>
-    <SnackbarSuccess/>
-    <SnackbarError/>
+    <SnackbarSuccess />
+    <SnackbarError />
     <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import SnackbarSuccess from './components/core/SnackbarSucess.vue';
-import SnackbarError from './components/core/SnackbarError.vue';
+import SnackbarSuccess from "./components/core/SnackbarSucess.vue";
+import SnackbarError from "./components/core/SnackbarError.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     SnackbarSuccess,
-    SnackbarError,
+    SnackbarError
   },
 
-  mounted(){
-    if(!this.$store.state.usuario){
-      return this.$router.push({'name': 'login'});
+  mounted() {
+    if (!this.$store.state.usuario) {
+      return this.$router.push({ name: "login" });
     }
   }
 };
 </script>
 
 <style>
-  .meio {
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-  }
-  .snack {
-    animation-duration: 600ms;
-  }
-  .expandeDiv {
-    display: flex;
-    width: 100%;
-    flex-wrap: wrap;
-  }
-  .linhaSemQuebra {
-    display: flex;
-    width: 100%;
-    flex-wrap: nowrap;
-    align-items: center;
-    height: 50px;
-  }
-
+.meio {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+}
+.snack {
+  animation-duration: 600ms;
+}
+.expandeDiv {
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+}
+.linhaSemQuebra {
+  display: flex;
+  width: 100%;
+  flex-wrap: nowrap;
+  align-items: center;
+  height: 50px;
+}
 </style>
