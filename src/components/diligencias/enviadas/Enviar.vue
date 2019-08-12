a1531747cb17ab35bd14947ce5beb56fcb0e92ee
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-08-12 12:51:24 -0300

    alteração no estilo dos inputs do form

--- src/components/diligencias/enviadas/Enviar.vue
+++ src/components/diligencias/enviadas/Enviar.vue
@@ -1,17 +1,16 @@
 <template>
   <v-container fluid>
     <div class="linhaSemQuebra">
-      <h3 style="text-align:center">
+      <h3 class="pa-1 mt-3">
         Envie sua diligência:
       </h3>
     </div>
-    <v-card class="pa-3">
+    <v-card class="pa-5 mt-3">
       <v-layout row>
         <v-flex xs12 md4 pa-2>
           <v-text-field
           label="Nome:"
           placeholder="Qual o seu nome?"
-          filled
           >
 
           </v-text-field>
@@ -20,7 +19,6 @@
           <v-text-field
           label="Celular:"
           placeholder="Qual o seu celular?"
-          filled
           >
 
           </v-text-field>
@@ -29,7 +27,6 @@
           <v-text-field
           label="E-mail:"
           placeholder="Qual o seu e-mail?"
-          filled
           >
 
           </v-text-field>
@@ -40,7 +37,6 @@
           <v-autocomplete
           label="Serviços:"
           placeholder="Serviço que deseja solicitar"
-          filled
           />
 
         </v-flex>
@@ -48,14 +44,12 @@
          <v-autocomplete
           label="Cidades:"
           placeholder="Qual a sua cidade?"
-          filled
           />
         </v-flex>
         <v-flex xs12 md3 pa-2>
           <v-text-field
           label="Data:"
           placeholder="Em que dia:"
-          filled
           >
 
           </v-text-field>
@@ -64,7 +58,6 @@
           <v-text-field
           label="Horário:"
           placeholder="Em qual horário?"
-          filled
           >
 
           </v-text-field>
@@ -73,7 +66,6 @@
       <v-layout row>
         <v-flex xs12 md12 pa-2>
           <v-textarea
-            filled
             label="Fale um pouco mais sobre sua diligência:"
           >
 
