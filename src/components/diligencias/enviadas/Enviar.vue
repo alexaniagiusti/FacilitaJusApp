d02cecb1a5320c449a27d181da0c664f3ce41cd3
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-08-12 20:59:49 -0300

    validação do form da tela enviar diligencia

--- src/components/diligencias/enviadas/Enviar.vue
+++ src/components/diligencias/enviadas/Enviar.vue
@@ -10,6 +10,7 @@
         <v-flex xs12 md4 pa-2>
           <v-text-field
           label="Nome:"
+          v-model="name"
           placeholder="Qual o seu nome?"
           >
 
@@ -18,6 +19,7 @@
         <v-flex xs12 md4 pa-2>
           <v-text-field
           label="Celular:"
+          v-model="phone"
           placeholder="Qual o seu celular?"
           >
 
@@ -26,6 +28,7 @@
         <v-flex xs12 md4 pa-2>
           <v-text-field
           label="E-mail:"
+          v-model="email"
           placeholder="Qual o seu e-mail?"
           >
 
@@ -35,20 +38,50 @@
       <v-layout row>
         <v-flex xs12 md3 pa-2>
           <v-autocomplete
-          label="Serviços:"
+          label="Serviço:"
+          :items="services"
+          v-model="serviceSelected"
+          item-value="id"
+          item-text="service"
+          hide-no-data
           placeholder="Serviço que deseja solicitar"
           />
 
         </v-flex>
         <v-flex xs12 md3 pa-2>
          <v-autocomplete
-          label="Cidades:"
-          placeholder="Qual a sua cidade?"
-          />
+							v-model="city"
+							:items="cities"
+							hide-no-data
+							return-object
+							label="Cidade"
+              placeholder="Cidade"
+							item-text="city"
+							item-value="id"
+						>
+							<template v-slot:selection="data">
+									{{ data.item.city }} - {{ data.item.state }}
+							</template>
+							<template v-slot:item="data">
+								<template v-if="typeof data.item !== 'object'">
+									<v-list-item-content v-text="data.item.city"></v-list-item-content>
+								</template>
+								<template v-else>
+									<v-list-item-avatar class="elevation-1">
+										<v-icon>place</v-icon>
+									</v-list-item-avatar>
+									<v-list-item-content>
+										<v-list-item-title v-html="data.item.city"></v-list-item-title>
+										<v-list-item-subtitle v-html="data.item.state"></v-list-item-subtitle>
+									</v-list-item-content>
+								</template>
+							</template>
+						</v-autocomplete>
         </v-flex>
         <v-flex xs12 md3 pa-2>
           <v-text-field
           label="Data:"
+          v-model="date"
           placeholder="Em que dia:"
           >
 
@@ -57,6 +90,7 @@
         <v-flex xs12 md3 pa-2>
           <v-text-field
           label="Horário:"
+          v-model="hour"
           placeholder="Em qual horário?"
           >
 
@@ -66,6 +100,7 @@
       <v-layout row>
         <v-flex xs12 md12 pa-2>
           <v-textarea
+            v-model="message"
             label="Fale um pouco mais sobre sua diligência:"
           >
 
@@ -75,6 +110,7 @@
           <v-btn
             block
             color="green"
+            @click="sendDiligence"
           >
             Enviar
           </v-btn>
@@ -83,3 +119,64 @@
     </v-card>
   </v-container>
 </template>
+
+<script>
+import axios from 'axios'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+export default {
+  data() {
+    return {
+      name: '',
+      phone: '',
+      email: '',
+      date: '',
+      dateFormat: '',
+      hour: '',
+      message: '',
+      cities: [],
+      citySelected: '',
+      services: [],
+      serviceSelected: ''
+    }
+  },
+  methods: {
+    getServices() {
+      axios.get(this.$store.getters.api + '/api/v1/services',{ headers: { Authorization: `Bearer ${this.$store.getters.getToken}` }})
+        .then(res => this.services = res.data)
+    },
+    getCities() {
+      axios.get(this.$store.getters.api + '/api/v1/cities',{ headers: { Authorization: `Bearer ${this.$store.getters.getToken}` }})
+        .then(res => this.cities = res.data)
+    },
+    sendDiligence() {
+      const data = {
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+        message: this.message,
+        service_id: this.serviceSelected,
+        city_id: this.citySelected,
+        time: this.hour,
+        date: this.dateFormat
+      }
+
+      axios.post(`${this.$store.getters.api}/api/v1/diligences`, data, { headers: { Authorization: `Bearer ${this.$store.getters.getToken}` } })
+        .then( () => this.$store.dispatch('snackbar_success', 'Diligência enviada com sucesso') )
+        .catch(() => this.$$store.dispatch('snackbar_error', 'Erro, tente novamente'))
+    }
+  },
+  created() {
+    this.name = this.$store.getters.getUsuario.name
+    this.phone = this.$store.getters.getUsuario.phone_1
+    this.email = this.$store.getters.getUsuario.email
+    this.date = moment().format('DD[/]MM[/]YYYY')
+    this.dateFormat = moment().format('YYYY[-]MM[-]DD')
+    this.hour = moment().format('HH:mm')
+    // pega os serviços e as cidades ao iniciar o componente para carregá-los nos selects
+    this.getServices()
+    this.getCities()
+  }
+}
+</script>
