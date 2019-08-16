f6c9c0417b29d5cfc2f8a97c96e94faeb9092aaa
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-08-16 16:34:11 -0300

    adição do autocomplete off nos formulários

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store/index';
 import './registerServiceWorker';
+import DisableAutocomplete from 'vue-disable-autocomplete';
 
 import '@babel/polyfill';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
@@ -12,6 +13,7 @@ import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(rtdbPlugin);
 Vue.config.productionTip = false;
+Vue.use(DisableAutocomplete);
 
 
 new Vue({
