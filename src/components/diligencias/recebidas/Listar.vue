a33140acbb43c96d1714df2e802a86fcf2644cf2
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-08-21 15:49:15 -0300

    adição da lista da visualização mobile

--- src/components/diligencias/recebidas/Listar.vue
+++ src/components/diligencias/recebidas/Listar.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-layout>
 		<v-flex xs12>
-			<v-simple-table>
+			<v-simple-table class="hidden-sm-and-down">
 				<thead>
 					<tr>
 						<th class="text-left blue--text">#</th>
@@ -27,6 +27,35 @@
 					</tr>
 				</tbody>
 			</v-simple-table>
+			<v-list three-line class="hidden-sm-and-up">
+				<v-list-item v-for="diligence in diligences" :key="diligence.id" @click="showDiligence(diligence.id, diligence.name)">
+					<v-avatar class="pr-2">
+						<span>#{{diligence.id}}</span>
+					</v-avatar>
+					<v-list-item-content>
+						<v-list-item-title>
+							<span class="font-weight-bold">{{diligence.name | nameFilter}}</span>
+						</v-list-item-title>
+							{{ diligence.service.service }}
+						<v-list-item-title>
+							{{ diligence.date | dateFilter }} - 	{{ diligence.time }}
+						</v-list-item-title>
+						<v-list-item-subtitle>
+							<v-icon size="15">
+								place
+							</v-icon>
+							<span> {{ diligence.city.city }} - {{ diligence.city.state }}</span>
+						</v-list-item-subtitle>
+					</v-list-item-content>
+					<v-list-item-action>
+						<v-btn icon @click="showDiligence(legalCase.id)">
+							<v-icon>
+								visibility
+							</v-icon>
+						</v-btn>
+					</v-list-item-action>
+				</v-list-item>
+			</v-list>
 		</v-flex>
 	</v-layout>
 </template>
