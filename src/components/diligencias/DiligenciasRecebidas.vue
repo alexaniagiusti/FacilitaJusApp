c167992ab6f7be50b04a936490c2bdabdf200263
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-09-19 12:18:10 -0300

    Alteração no titulo do toolbar

--- src/components/diligencias/DiligenciasRecebidas.vue
+++ src/components/diligencias/DiligenciasRecebidas.vue
@@ -4,7 +4,7 @@
 			<v-flex>
 				<v-toolbar flat color="white" dark>
 					<v-spacer></v-spacer>
-					<v-toolbar-title class="font-weight-light black--text">Serviços Recebidos</v-toolbar-title>
+					<v-toolbar-title class="font-weight-light black--text">Serviços que você está habilitado para atender:</v-toolbar-title>
 					<v-spacer></v-spacer>
 				</v-toolbar>
 
